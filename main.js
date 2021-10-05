d9c334522c7683278b8decfdc0f54de8ea5ddf41
gayatrieee <88025844+gayatrieee@users.noreply.github.com>
2021-10-05 18:32:36 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+menu_list_array = ["Veg Margherita Pizza,","cheeze pizza","Pepeoroni pizza"]
+,//add more items
+                    ];
+
+function getmenu(){
+var htmldata;
+menu_list_array.sort();
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+ menu_list_array[i] + '<br>'
+        }
+        document.getElementById("display_menu").innerHTML = htmldata;
+        //give the appropriate id name as display_menu  
+}
+
+function add_item(){
+var htmldata;
+var item=document.getElementById("add_item").value;
+      menu_list_array.push(item);
+      menu_list_array.sort();
+        htmldata="<section class='cards'>"
+        for(var i=0;i<menu_list_array.length;i++){
+            htmldata=htmldata+imgtags+ menu_list_array[i]+'<br>';
+        }
+        
+         document.getElementById("display_addedmenu").innerHTML = htmldata;		
+      }
+
+function add_top(){
+    var item=document.getElementById("add_item").value;
+    menu_list_array.push(item);
+	add_item();
+}
